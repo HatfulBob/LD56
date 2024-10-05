8cdd94cb7ae2bf53f152d1ae8c62782f7aa98db5
Lazlo319 <lazlogaming@gmail.com>
2024-10-05 23:19:43 +0200

    main menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject levelSelectScreen;
+    public void StartGame()
+    {
+        //GO TO FIRST SCENE
+        SceneManager.LoadScene("InProgress");
+    }
+
+    public void LevelSelect()
+    {
+        levelSelectScreen.SetActive(true);
+
+    }
+
+    public void LevelSelectBack()
+    {
+        levelSelectScreen.SetActive(false);
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
